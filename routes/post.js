4a8182d6339717378cdbd35078c8fe9f077e2a15
ossamuiux <dalhoya@naver.com>
2025-07-17 16:27:59 +0900

    fix: post api 수정

--- routes/post.js
+++ routes/post.js
@@ -9,33 +9,53 @@ router.get('/post', (req, res) => {
   const limit = 10;
   const offset = (currentPage - 1) * limit;
 
-  let query, params;
+  let query, params, countQuery, countParams;
+
   if (!category) {
-    query = `SELECT *, DATE_FORMAT(created_at, '%Y.%m.%d %H:%m:%s') format_date
+    countQuery = `SELECT COUNT(*) AS total FROM post`;
+    query = `SELECT post_id, category_id, title, views,
+            DATE_FORMAT(created_at, '%Y.%m.%d %H:%i:%s') format_date
             FROM post
             ORDER BY created_at DESC
             LIMIT ? OFFSET ?`;
     params = [limit, offset];
+    countParams = [];
   } else {
-    query = `SELECT post.*, category.category_name
+    countQuery = `SELECT COUNT(*) AS total
+                  FROM post
+                  JOIN category ON post.category_id = category.category_id
+                  WHERE category.category_name = ?`;
+    query = `SELECT post.post_id, post.category_id, post.title, post.views, category.category_name,
+            DATE_FORMAT(created_at, '%Y.%m.%d %H:%i:%s') format_date
             FROM post
             JOIN category ON post.category_id = category.category_id
             WHERE category.category_name = ?
+            ORDER BY post.created_at DESC
             LIMIT ? OFFSET ?`;
     params = [category, limit, offset];
+    countParams = [category];
   }
 
-  connection.query(query, params, (err, result) => {
-    if (err) {
-      console.error('가져오기 에러: ', err.message);
+  connection.query(countQuery, countParams, (countErr, countResult) => {
+    if (countErr) {
+      console.error('데이터 갯수 조회 에러: ', countErr.message);
       return res.status(500).send('데이터베이스 에러');
     }
 
-    if (result.length === 0) {
-      return res.status(404).json({ message: '데이터 없음' });
-    }
+    const total = countResult[0]?.total;
 
-    res.json(result);
+    connection.query(query, params, (err, result) => {
+      if (err) {
+        console.error('데이터 가져오기 에러: ', err.message);
+        return res.status(500).send('데이터베이스 에러');
+      }
+
+      if (result.length === 0) {
+        return res.status(404).json({ message: '데이터 없음' });
+      }
+
+      res.json({ total, data: result });
+    });
   });
 });
 
